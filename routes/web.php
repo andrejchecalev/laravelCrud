5b5d576e373a2449dc8bd9667f0ace8e13293dc9
andrey <andrej.checalev@gmail.com>
2022-11-09 20:59:57 +0200

    commit lab2

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-Route::redirect('/','users');
-Route::resource("users",UsersController::class);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('/users',\App\Http\Controllers\UsersController::class)
+->middleware('auth');
+
+Route::resource('usersList', \App\Http\Controllers\UsersController::class);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
